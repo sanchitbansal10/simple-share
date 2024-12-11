05ce1d40e37196e20d9abeee8a0cb0cd09485c46
Sanchit Bansal <sanchitbansal10@gmail.com>
2024-12-11 17:36:22 +0530

    fix

--- src/app/[noteId]/page.jsx
+++ src/app/[noteId]/page.jsx
@@ -2,17 +2,14 @@ import EditorWrapper from '../__components/EditorWrapper';
 
 export default async function NotePage({ params }) {
     const noteId = (await params).noteId;
-    
-    const response = await fetch(`http://localhost:3000/api/notes?id=${noteId}`, {
+    console.log({noteId});
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '';
+    const response = await fetch(`${baseUrl}/api/notes?id=${noteId}`, {
         cache: 'no-store',
         headers: {
             'Content-Type': 'application/json',
         },
     });
-
-    if (!response.ok) {
-        throw new Error(`Failed to fetch note: ${response.statusText}`);
-    }
 
     const editorData = await response.json();
     
